2cf7556b479868d6610509c3564f63fe2032f822
miltonz0628 <67836330+miltonz0628@users.noreply.github.com>
2023-03-07 21:47:26 -0500

    Update users.js

--- backend/data/users.js
+++ backend/data/users.js
@@ -6,12 +6,13 @@ const { ObjectId } = require("mongodb");
 const data = require(".");
 
 
-function BankRequest(contractor_id, project_id) {
+
+function isApproved(contractor_id, project_id) {
   for(let i = 0; i < contractors.length; i++){
     if(contractor_id === contractors[i].id){
-        for(let n = 0; n < contractors[i].BankPayment.length; n++){
-            if(project_id === contractors[i].BankPayment[n].projectId){
-                if(contractors[i].BankPayment[n].approved){
+        for(let n = 0; n < contractors[i].bankPayment.length; n++){
+            if(project_id === contractors[i].bankPayment[n].projectId){
+                if(contractors[i].bankPayment[n].approved){
                     return true
                 }
                 else{
@@ -26,9 +27,44 @@ function BankRequest(contractor_id, project_id) {
 
 }
 
+function bankRequest(contractor_id, project_id){
+    return "I am requesting to know if my project has been funded."
+    const randomNum = Math.round(Math.random());
+    if(randomNum === 0){
+        return "it is approved"
+    }
+    else{
+        return "it is not approved"
+    }
+
+}
+
+
+function updateDB(contractor_id, project_id){
+    for(let i = 0; i < contractors.length; i++){
+        if(contractor_id === contractors[i].id){
+            for(let n = 0; n < contractors[i].bankPayment.length; n++){
+                if(project_id === contractors[i].bankPayment[n].projectId){
+                    if(contractors[i].bankPayment[n].approved){
+                        return "project is already approved"
+                    }
+                    else{
+                        contractors[i].bankPayment[n].approved = true
+                        return "Approval status has been updated"
+                    }
+                }
+            }
+            throw "Project id not found"
+        }
+      }
+      throw "Contractor id not found"
+
+}
 
 module.exports = {
-  BankRequest
+  isApproved,
+  bankRequest,
+  updateDB
 };
 
 /*
