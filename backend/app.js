ba2bfe5ca3a2af348c0efbd16afb0613cc72cc43
miltonz0628 <67836330+miltonz0628@users.noreply.github.com>
2023-03-07 23:30:26 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
-const app = express();
 const configRoutes = require("./routes");
+const data = require("./data");
 
-app.use(express.json());
+data.contractors.createContractor(
+  "venkat",
+  "venkat@stevens.edu",
+  [
+    { from: "ssdfijiausrhfij", text: "message from a friend" },
+    { from: "ssdfijiausrhfij", text: "Another message from a friend" },
+  ],
+  [
+    { projectId: "ksjdhfihasdkifgjhlkj", tasks: ["task 1", "task2, task3"] },
+    {
+      projectId: "ksjdhfihasdkifgjhlkj",
+      tasks: ["task 2.1", "task2.2, task2.3"],
+    },
+  ],
+  "datetime here"
+);
 
-configRoutes(app);
 
-app.listen(3000, () => {
-  console.log("We've now got a server!");
-  console.log("Your routes will be running on http://localhost:3000");
-});
