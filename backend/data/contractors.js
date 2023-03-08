c0ddfb6bb349a988def2ac6673e02f7d7f584aeb
miltonz0628 <67836330+miltonz0628@users.noreply.github.com>
2023-03-07 23:31:15 -0500

    Update contractors.js

--- backend/data/contractors.js
+++ backend/data/contractors.js
@@ -4,11 +4,27 @@ const projects = mongoCollections.projects;
 const contractors = mongoCollections.contractors;
 const { ObjectId } = require("mongodb");
 const data = require(".");
+async function createContractor(name, email, messages, todo, calender) {
+  const contractorCollection = await contractors();
+  id = new ObjectId();
 
-const helloWorld = function helloWorld() {
-  console.log("Hello world");
-};
+  let newContractor = {
+    _id: id,
+    name: name,
+    email: email,
+    messages: messages,
+    todo: todo,
+    calender: calender,
+  };
+
+  const insertInfo = await contractorCollection.insertOne(newContractor);
+  if (!insertInfo.acknowledged) {
+    throw "please try adding again";
+  }
+  return newContractor;
+}
 
 module.exports = {
-  helloWorld,
+  createContractor,
 };
+
