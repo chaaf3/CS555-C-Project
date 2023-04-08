ad4cd81fbb0abd6a87c97ee026194f88a4e5a5a3
chaaf <chaaf@stevens.edu>
2023-04-07 22:22:13 -0400

    handled auth forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Scheduler } from "@aldabil/react-scheduler";
 import Calendar from "./Components/Calendar";
-import Auth from "./Components/Auth";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 
 const App = () => {
   return (
     <div>
-      <h1>Schedule your appointments</h1>
-      <Calendar />;
+      <SignUp />;
     </div>
   );
 };
