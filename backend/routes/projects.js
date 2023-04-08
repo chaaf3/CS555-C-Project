3edfb16d62a559eefe7da1b69161cfd40746dbc5
Edward Chung <55555305+edwardchungg@users.noreply.github.com>
2023-04-07 23:28:00 -0400

    Added Expandable Project LIst
    List of projects and their respective queue of tasks to be done using Material UI

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -4,4 +4,16 @@ const data = require("../data");
 const projectData = data.projects;
 // const validation = require("../validation");
 
+router.get("/:projectId", async (req, res) => {
+  try {
+    let id = req.params.projectId;
+    console.log(id);
+    let project = await projectData.getProject(id);
+    console.log(project);
+    res.send(project);
+  } catch (e) {
+    res.status(404).json({ error: e });
+  }
+});
+
 module.exports = router;
